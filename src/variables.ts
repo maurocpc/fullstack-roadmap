914abb48fb1d1d715de5ca6c8917730c064532ec
Mauro Cruz <maurocpcruz@gmail.com>
2026-01-06 19:18:45 -0300

    Add variables intro

---
+++ src/variables.ts
@@ -0,0 +1,13 @@
+// 1. String (Text)
+const city: string = "São Paulo"; 
+
+// 2. Number (Integers or Decimals)
+let temperature: number = 25.5; 
+
+// 3. Boolean (True or False - like a light switch)
+const isRaining: boolean = false;
+
+// 4. Array (A list of items)
+let tools: string[] = ["VS Code", "Git", "TypeScript"];
+
+console.log(`In ${city}, it is ${temperature} degrees.`);
