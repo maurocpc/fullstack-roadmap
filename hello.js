c19c8e7987e765f9994fb3b3830da04e041dacfe
Mauro Cruz <maurocpcruz@gmail.com>
2026-01-06 18:31:14 -0300

    Refactor: Move source to src and ignore compiled files

--- hello.js
+++
@@ -1,8 +0,0 @@
-// This is a variable. We use 'const' for values that don't change.
-// We tell TypeScript this is a 'string' (text).
-var teacherName = "Gemini";
-// This is a function. It takes a name and returns a greeting.
-function greet(name) {
-    return "Hello, ".concat(name, "! Welcome to your first TypeScript lesson.");
-}
-console.log(greet(teacherName));
